97c09b6efa7ab04242577af63b2e305f307c2da0
Alex Collino <arcollino@gmail.com>
2022-03-08 11:23:50 -0800

    Update html to use standalone Jasmine, update afterEach(), add test for updateServerTable()

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,17 +1,31 @@
 describe("Servers test (with setup and tear-down)", function() {
   beforeEach(function () {
     // initialization logic
-    serverNameInput.value = 'Alice';
+    serverNameInput.value = "Alice";
   });
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
+  it("should add a new server to allServers on submitServerInfo()", function () {
     submitServerInfo();
 
     expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    expect(allServers["server" + serverId].serverName).toEqual("Alice");
+  });
+
+  it("should add each server in allServers to updateServerTable()", function () {
+    allServers = {
+      server1: "Alice",
+      server2: "Mark",
+      server3: "George",
+      server4: "Lisa",
+    };
+    updateServerTable();
+    expect(serverTbody.children.length).toEqual(4);
   });
 
   afterEach(function() {
-    // teardown logic
+    serverTbody.innerHTML = "";
+    serverNameInput.value = "";
+    allServers = {};
+    serverId = 0;
   });
 });
